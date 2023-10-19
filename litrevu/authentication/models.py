7e19f9613b02fb72fbdf83179e061b97f15b7ce7
DayriseA_dom <adrien.maisak@gmail.com>
2023-10-20 01:12:44 +0200

    feat(authentication): add CustomUser model; registered to admin site
    requirements updated with Pillow; initial migrations made; superuser 'dayrise' created; groups 'basic' and 'administrators' created

--- litrevu/authentication/models.py
+++ litrevu/authentication/models.py
@@ -1,3 +1,41 @@
+from django.conf import settings
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+def get_avatar_path(instance, filename):
+    return f"user_{instance.id}/avatars/{filename}"
+
+
+class CustomUser(AbstractUser):
+    avatar = models.ImageField(
+        upload_to=get_avatar_path, verbose_name="Avatar", null=True, blank=True
+    )
+    follows = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        through="UserFollows",
+        related_name="followers",
+        verbose_name="suit",
+        symmetrical=False,
+        blank=True,
+    )
+    blocks = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        blank=True,
+    )
+
+
+class UserFollows(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="following",
+    )
+    followed_user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="followed_by",
+    )
+
+    class Meta:
+        unique_together = ("user", "followed_user")
