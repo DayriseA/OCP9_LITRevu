075168c51769dc6fac0e7dfd2a6f7d7b87d962cc
DayriseA_dom <adrien.maisak@gmail.com>
2023-10-23 11:35:02 +0200

    feat(rating): create model Review; make and run migrations

--- litrevu/rating/models.py
+++ litrevu/rating/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.conf import settings
 from django.db import models
 
@@ -24,3 +25,16 @@ class Ticket(models.Model):
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
         self.resize_image()
+
+
+class Review(models.Model):
+    ticket = models.ForeignKey(Ticket, on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    rating = models.PositiveSmallIntegerField(
+        # validates that rating must be between 0 and 5
+        validators=[MinValueValidator(0), MaxValueValidator(5)],
+        verbose_name="Note",
+    )
+    headline = models.CharField(max_length=128, verbose_name="Titre")
+    body = models.TextField(max_length=8192, blank=True, verbose_name="Commentaire")
+    time_created = models.DateTimeField(auto_now_add=True)
